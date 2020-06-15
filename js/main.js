69aa6c948cbacee288019fdf227534ea8276e4db
mo3az <51883008+mo3az96@users.noreply.github.com>
2020-06-15 14:17:52 +0200

    logo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(window).on('load', function () {
-    $('.pre-loader').fadeOut("500", function () {
+    $('.pre-loader').delay(4250).fadeOut("500", function () {
         $(this).remove();
     });
     $(".mo-modal-overlay").fadeIn(500)
